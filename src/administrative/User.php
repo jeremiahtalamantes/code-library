40c17a6e2c0a2bbcbdd9c47364cf56b56de40d35
JT <neobotnet@gmail.com>
2023-03-10 11:38:28 -0600

    update class

--- src/administrative/User.php
+++ src/administrative/User.php
@@ -57,7 +57,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         {
             return $this->first_name;
         }
-        
+
+        public function setLastName($last_name)
+        {
+            $this->last_name = $last_name;
+        }
+
+        public function getLastName()
+        {
+            return $this->last_name;
+        }
+
+        public function setUUID($uuid)
+        {
+            $this->uuid = $uuid;
+        }
+
+        public function getUUID()
+        {
+            return $this->uuid;
+        }
+
+
 
         /**
          * Member Functions
@@ -66,6 +87,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
         private function _generateUUID()
         {
+            /**
+             * Similar to UUID v4
+             */
 
+            $data = $data ?? random_bytes(16);
+            assert(strlen($data) == 16);        
+            $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+            $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+        
+            // Return UUID
+            return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
         }
     }
