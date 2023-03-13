8e202078f6e7f6bb5221c651732800c685eb62a8
JT <neobotnet@gmail.com>
2023-03-13 11:03:19 -0500

    add

---
+++ src/administrative/Comment.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * 
+ * 
+ * Comment.php | OOP Class for CI 3
+ * 
+ * 
+ */
+
+ class Comment {
+
+    /**
+     * Properties
+     */
+    private $comment;
+
+    /**
+     * Setters & Getters
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Methods
+     */
+
+ }
